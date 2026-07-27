8d7ab6000957f326fda3e426a5d38bb5586c5067
dluman <dluman@allegheny.edu>
2026-07-27 19:42:49 -0400

    Only original programs now that ALU tests OK

--- src/byte.s
+++ src/byte.s
@@ -2,10 +2,8 @@
 0x0001   0x18   ; CLC — clear carry flag
 0x0002   0xA9   ; LDA immediate — load 1st number
 0x0003   0x05   ; Value: $05
-0x0004   0x18   ; CLC — clear carry flag
 0x0005   0x69   ; ADC immediate — add 2nd number
 0x0006   0x0A   ; Value: $0A
-0x0007   0x18   ; CLC — clear carry flag
 0x0008   0x69   ; ADC immediate — add 3rd number
 0x0009   0x0F   ; Value: $0F
 0x000A   0x8D   ; STA absolute — store partial sum to RAM
@@ -13,7 +11,6 @@
 0x000C   0x02   ; Address high byte: $02 (target: $0200)
 0x000D   0xA9   ; LDA immediate — load 4th number
 0x000E   0x14   ; Value: $14
-0x000F   0x18   ; CLC — clear carry flag
 0x0010   0x6D   ; ADC absolute — add partial sum from RAM
 0x0011   0x00   ; Address low byte: $00
 0x0012   0x02   ; Address high byte: $02 (source: $0200)
